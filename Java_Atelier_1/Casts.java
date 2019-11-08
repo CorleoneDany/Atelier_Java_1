738b84c8d5698f44d4ad987e4fbb1105601611ac
Corleone <corleonedany@live.fr>
2019-11-08 12:02:19 +0100

    Exercice 5 complété

--- Java_Atelier_1/Casts.java
+++ Java_Atelier_1/Casts.java
@@ -3,11 +3,10 @@ package Java_Atelier_1;
 public class Casts {
 	void int_to_ASCII() {
 		int yourInt = 33;
-		char ascii_char = (char) yourInt;
-		while(yourInt < 127) {
+		while (yourInt < 127) {
+			char ascii_char = (char) yourInt;
 			System.out.println("The ASCII representation of " + yourInt + " is : " + ascii_char);
 			yourInt++;
 		}
-
 	}
 }
